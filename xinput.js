d519c10b9b136814dc2077e38630b2a17200afa6
Norman Xu <homyu.shinn@gmail.com>
2011-11-18 18:59:41 +0800

    bug fixing

--- xinput.js
+++ xinput.js
@@ -5,7 +5,9 @@
 */
 ;!function($, etui){
 
-	var var DATA_KEY_OPTS = 'jQuery.fn.xinput',
+	$ = etui.$;
+
+	var DATA_KEY_OPTS = 'jQuery.fn.xinput',
 		DATA_KEY_XINPUT = 'jQuery.fn.xinput.prevText';
 
 	var defaultSettings = {
@@ -15,29 +17,39 @@
 	var init = function(){
 		var $el = $(this);
 		var opts = $el.data(DATA_KEY_OPTS);
+		var callback = opts.callbacks[opts.callbacks.length - 1];
 
 		$el
 			.bind('input', opts.callback)
         	.bind('propertychange',(function(callback){
             	return function(){
-	                var el = $(this);
-	                var pt = el.data(DATA_KEY_XINPUT);
-	                el.data(DATA_KEY_XINPUT, el.val());
-	                if (pt !== el.val()){
+	                var $el = $(this);
+	                var pt = opts.prevText;
+	                opts.prevText = $el.val();
+	                
+	                if (pt !== $el.val()){
 	                    callback.apply(this, arguments);
 	                }
             	};
-        	})(opts.callback))
-        	.data(DATA_KEY_XINPUT, el.val());
+        	})(callback));
+        	opts.prevText = $el.val();
 	};
 
 	var exports = {
 		xinput: function(callback){
-			var opts = $.extend({}, defaultSettings, {
-				callback: callback
-			});
+			
 
-			this.data(DATA_KEY_OPTS, opts);
+			var opts = this.data(DATA_KEY_OPTS);
+			if (!opts){
+				opts = {callbacks: []};
+				this.data(DATA_KEY_OPTS, opts);
+			}
+
+			if (!opts.callbacks){
+				opts.callbacks = [];
+			}
+
+			opts.callbacks.push(callback);
 			
 			return this.each(init);
 		}
