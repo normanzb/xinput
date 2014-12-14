8e7812ab02e4810eff3d13eee5b916796f2da652
Norman Xu <homyu.shinn@gmail.com>
2014-12-14 21:25:57 +0000

    improved and simplified

--- xinput.js
+++ xinput.js
@@ -1,9 +1,9 @@
-if (typeof define !== 'function' && typeof module != 'undefined') {
-    var define = require('amdefine')(module);
-}
+define(['../../lib/boe/src/boe/Function/bind'], function (bind) {
+    'use strict';
 
-define(function () {
     var INPUT = 'input';
+    var CHANGE = 'change';
+    var PROPERTYNAME = 'propertyName';
 
     /* Feature Detection */
 
@@ -15,21 +15,21 @@ define(function () {
         */
         function checkEvent(el) {
             // First check, for if Firefox fixes its issue with el.oninput = function
-            el.setAttribute("oninput", "return");
-            if (typeof el.oninput == "function"){
+            el.setAttribute('oninput', 'return');
+            if (typeof el.oninput == 'function'){
                 return true;
             }
 
             // Second check, because Firefox doesn't map oninput attribute to oninput property
             try {
-                var e  = document.createEvent("KeyboardEvent"),
+                var e  = document.createEvent('KeyboardEvent'),
                     ok = false,
                     tester = function(e) {
                         ok = true;
                         e.preventDefault();
                         e.stopPropagation();
-                    }
-                e.initKeyEvent("keypress", true, true, window, false, false, false, false, 0, "e".charCodeAt(0));
+                    };
+                e.initKeyEvent('keypress', true, true, window, false, false, false, false, 0, 'e'.charCodeAt(0));
                 document.body.appendChild(el);
                 el.addEventListener(INPUT, tester, false);
                 el.focus();
@@ -37,48 +37,122 @@ define(function () {
                 el.removeEventListener(INPUT, tester, false);
                 document.body.removeChild(el);
                 return ok;
-            } catch(e) {}
+            } catch(ex) {}
         }
 
         var testee = document.createElement(INPUT);
-        return "oninput" in testee || checkEvent(testee);
+        return 'oninput' in testee || checkEvent(testee);
     }();
+
+    /* Private */
+
+    function onchange( evt ){
+        var me = this;
+        
+        if ( 
+            PROPERTYNAME in window.event && 
+            window.event[PROPERTYNAME] != null && 
+            window.event[PROPERTYNAME] !== ''
+        ) {
+            if ( window.event[PROPERTYNAME] !== 'value') {
+                return;
+            }
+        }
+        
+        if ( me._el.value != me._old ) {
+            me._old = me._el.value;
+            me.oninput( evt );
+        }
+    }
+
+    function onfocus () {
+        document.attachEvent('onselectionchange', this._onchange);
+    }
+
+    function onblur() {
+        document.detachEvent('onselectionchange', this._onchange);
+    }
+
+    function oninput(){
+        this.oninput();
+    }
 
     /* Public */
     
     function Observer(){
         this._old = '';
+        this._el = null;
+        this._onchange = bind.call(onchange, this);
+        this._onfocus = bind.call(onfocus, this);
+        this._onblur = bind.call(onblur, this);
+        this._oninput = bind.call(oninput, this);
         this.oninput = function(){};
     }
 
     var p = Observer.prototype;
 
+    p.trigger = function () {
+        return this._onchange();
+    };
+
+    p.sync = function () {
+        this._old = this._el.value;
+    };
+
     p.observe = function(el){
         if ( el == null || el.tagName.toLowerCase() != INPUT ) {
-            throw "Target input element must be specified.";
+            throw 'Target input element must be specified.';
         }
 
         var me = this;
+        me._el = el;
 
-        if ( hasOnInput ) {
-            el.addEventListener(INPUT, function() {
-                me.oninput();
-            }, false);
+        // higher priority to use prooperty change
+        // because IE9 oninput is not implemented correctly
+        // when you do backspace it doesn't fire oninput
+        if ( el.attachEvent ) {
+            me._old = el.value;
+            el.attachEvent('onpropertychange', me._onchange);
+            el.attachEvent('onfocus', me._onfocus);
+            el.attachEvent('onblur', me._onblur);
+            // binding onkeypress to avoid https://gist.github.com/normanzb/137a8b9d0cf317a1be58
+            el.attachEvent('onkeypress', me._onchange);
+            el.attachEvent('onkeyup', me._onchange);
         }
-        else if (el.attachEvent) {
-            this._old = el.value;
-            el.attachEvent('onpropertychange', function(evt){
-                if ( evt.propertyName == "value" && el.value != this._old ) {
-                    this._old = el.value;
-                    me.oninput();
-                }
-            });
+        else if ( hasOnInput ) {
+            el.addEventListener(INPUT, me._onchange, false);
+            // monitor onchange event as well just in case chrome browser bugs:
+            // https://code.google.com/p/chromium/issues/detail?id=353691
+            el.addEventListener(CHANGE, me._onchange, false);
         }
         else {
-            throw "Something wrong, should never goes to here.";
+            throw 'Something wrong, should never goes to here.';
         }
     };
 
-    return Observer;
+    p.neglect = function (){
+        var me = this;
+        var el = me._el;
 
+        if ( el.attachEvent ) {
+            el.detachEvent('onpropertychange', me._onchange);
+            el.detachEvent('onfocus', me._onfocus);
+            el.detachEvent('onblur', me._onblur);
+            el.detachEvent('onkeypress', me._onchange);
+            el.detachEvent('onkeyup', me._onchange);
+        }
+        else {
+            el.removeEventListener(INPUT, me._onchange);
+            el.removeEventListener(CHANGE, me._onchange);
+        }
+
+    };
+
+    p.dispose = function() {
+        var me = this;
+        me.neglect();
+        me._el = null;
+    };
+
+    return Observer;
 });
